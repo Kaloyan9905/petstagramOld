0e06848ae0d5d091e6f27fbfd897bc07472eed25
Kaloyan <milchevkaloian@gmail.com>
2023-04-28 10:45:31 +0300

    added some features

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = (
+from django.urls import path, include
 
+from petstagram.accounts.views import register_user, login_user, details_page, edit_page, delete_page
+
+urlpatterns = (
+    path('register/', register_user, name='register user'),
+    path('login/', login_user, name='login user'),
+    path('profile/', include([
+        path('<int:pk>', details_page, name='details page'),
+        path('<int:pk>/edit/', edit_page, name='edit page'),
+        path('<int:pk>/delete/', delete_page, name='delete page')
+    ]))
 )
--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def login_user(request):
+    return render(request, 'account/login-page.html')
+
+
+def register_user(request):
+    return render(request, 'account/register-page.html')
+
+
+def delete_page(request):
+    return render(request, 'account/profile-delete-page.html')
+
+
+def details_page(request):
+    return render(request, 'account/profile-details-page.html')
+
+
+def edit_page(request):
+    return render(request, 'account/profile-edit-page.html')
